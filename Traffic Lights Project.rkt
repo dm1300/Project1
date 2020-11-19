34c873258eab6a46d2874963c46561b3565edee4
dm1300 <60429144+dm1300@users.noreply.github.com>
2020-11-19 20:01:44 +0000

    Update Traffic Lights Project.rkt

--- Traffic Lights Project.rkt
+++ Traffic Lights Project.rkt
@@ -1,40 +1,50 @@
 #lang racket
 
 
-;States table
-;0 means red
-;1 means green
+;STATES TABLE
+
 
 (define StateTable
   '(
-    (1 (("a" 1) ("b" 0) ("c" 1) ("d" 0)))
-    (2 (("a" 0) ("b" 0) ("c" 0) ("d" 1)))
-    (3 (("a" 0) ("b" 1) ("c" 0) ("d" 0)))
-    (4 (("a" 0) ("b" 0) ("c" 0) ("d" 0)))
+    (1 (("A" "Green") ("B" "Red") ("C" "Green") ("D" "Red")))
+    (2 (("A" "Red") ("B" "Red") ("C" "Red") ("D" "Green")))
+    (3 (("A" "Red") ("B" "Green") ("C" "Red") ("D" "Red")))
+    (4 (("A" "Red") ("B" "Red") ("C" "Red") ("D" "Red")))
     ))
 
+;MAIN LOOP
+
+;this loop starts on the chosen state
 ;current state is x
-
+;it outputs the colour of each light for the state
+;then moves on to inbetween state
+;then to next full state
 (define State (λ (x) (cond
-                          (#t (cond    ;because this is always true, the loop doesn't stop
-;this loop checks what state the program is in
-                                ((= x 1)
-                                 (cond
-                                      ((= (first (rest (first (first (rest (first  StateTable)))))) 1) "Light A is Green") ;see comments bottom comments
-                                      (#t "Light A is Red")
-                                      ;I don't like having to write these two^^ lines up every time and have it clutter the program so I'll rewrite is and add a new 
-                                      ;lambda function before this lambda function and have it be called multiple times after
-                                      )
-                                 )
-                                ((= x 2) "")
-                                ((= x 3) "")
-                                ((= x 4) "")
-                                
-                                )
-
-
-                            
-                          ))))
+                           ((= x 1);colours of state 1
+                            (string-append "Light " (first (first (first (rest (first  StateTable))))) " is " (first (rest (first (first (rest (first  StateTable)))))))
+                            (string-append "Light " (first (second (first (rest (first  StateTable))))) " is " (first (rest (second (first (rest (first  StateTable)))))))
+                            (string-append "Light " (first (third (first (rest (first  StateTable))))) " is " (first (rest (third (first (rest (first  StateTable)))))))
+                            (string-append "Light " (first (fourth (first (rest (first  StateTable))))) " is " (first (rest (fourth (first (rest (first  StateTable)))))))
+                            )
+                           ((= x 2);colours of state 2
+                            (string-append "Light " (first (first (first (rest (second  StateTable))))) " is " (first (rest (first (first (rest (second  StateTable)))))))
+                            (string-append "Light " (first (second (first (rest (second  StateTable))))) " is " (first (rest (second (first (rest (second  StateTable)))))))
+                            (string-append "Light " (first (third (first (rest (second  StateTable))))) " is " (first (rest (third (first (rest (second  StateTable)))))))
+                            (string-append "Light " (first (fourth (first (rest (second  StateTable))))) " is " (first (rest (fourth (first (rest (second  StateTable)))))))
+                            )
+                           ((= x 3);colours of state 3
+                            (string-append "Light " (first (first (first (rest (third  StateTable))))) " is " (first (rest (first (first (rest (third  StateTable)))))))
+                            (string-append "Light " (first (second (first (rest (third  StateTable))))) " is " (first (rest (second (first (rest (third  StateTable)))))))
+                            (string-append "Light " (first (third (first (rest (third  StateTable))))) " is " (first (rest (third (first (rest (third  StateTable)))))))
+                            (string-append "Light " (first (fourth (first (rest (third  StateTable))))) " is " (first (rest (fourth (first (rest (third  StateTable)))))))
+                            )
+                           ((= x 4);colours of state 4
+                            (string-append "Light " (first (first (first (rest (fourth  StateTable))))) " is " (first (rest (first (first (rest (fourth  StateTable)))))))
+                            (string-append "Light " (first (second (first (rest (fourth  StateTable))))) " is " (first (rest (second (first (rest (fourth  StateTable)))))))
+                            (string-append "Light " (first (third (first (rest (fourth  StateTable))))) " is " (first (rest (third (first (rest (fourth  StateTable)))))))
+                            (string-append "Light " (first (fourth (first (rest (fourth  StateTable))))) " is " (first (rest (fourth (first (rest (fourth  StateTable)))))))
+                            )
+                          )))
 
 ;to see what colour each light should be in the StateTable, the following function is used:
 ;(NO (NO (light a-d (NO (NO (state 1-4 StateTable))))))
